883088580131dd3165189ba548bcfa202f508b7e
Fredy <fwhatley@frontlineed.com>
2017-01-05 21:18:51 -0500

    remove unneeded code

--- Oblivion.Api/App_Start/BundleConfig.cs
+++ Oblivion.Api/App_Start/BundleConfig.cs
@@ -8,7 +8,7 @@ namespace Oblivion
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+           /* bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
@@ -22,7 +22,7 @@ namespace Oblivion
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css"));*/
         }
     }
 }
--- Oblivion.Api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Oblivion.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
