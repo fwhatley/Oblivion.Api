6d80f9695c951cdc86489ac09f6a80b7020f6bae
Fredy <fwhatley@frontlineed.com>
2017-01-05 21:19:08 -0500

    add home page redirect to swagger

--- Oblivion.Api/App_Start/RouteConfig.cs
+++ Oblivion.Api/App_Start/RouteConfig.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Swashbuckle.Application;
 
 namespace Oblivion
 {
@@ -12,6 +14,13 @@ namespace Oblivion
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapHttpRoute(
+                name: "swagger_root",
+                routeTemplate: "",
+                defaults: null,
+                constraints: null,
+                handler: new RedirectHandler((message => message.RequestUri.ToString()), "swagger"));
 
             routes.MapRoute(
                 name: "Default",
